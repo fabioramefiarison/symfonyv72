ff4d9ec0f3c0a8b3d4770abf56c8019bfa991909
fabioramefiarison <fabioramefiarison@gmail.com>
2025-02-17 11:34:17 +0300

    installer twig

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class BlogController extends AbstractController
+{
+    #[Route('/blog', name: 'blog_list')]
+    public function list(): Response
+    {
+        return "bonnjour";
+    }
+}
+?>
